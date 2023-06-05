a6801a0a4473f7ecb3bf38ab224e50b03c2395dd
ccrs11 <ccrs11@hotmail.com>
2023-06-05 12:58:42 -0500

    feat: :tada: start with array_functions exercises

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad cómo utilizar las funciones de listas (arrays)</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p> Identificar planetas y sus respectivos números de orden en el sistema solar </p>
+    <form type='$_POST'>
+        <label for="orden">Ingresar el n-umero de orden</label>
+        <select id="orden" name="orden">Help
+            <option  name="1" id="1">1</option>
+            <option  name="2" id="2">2</option>
+            <option  name="3" id="3">3</option>
+            <option  name="4" id="4">4</option>
+            <option  name="5" id="5">5</option>
+            <option  name="6" id="6">6</option>
+            <option  name="7" id="7">7</option>
+            <option  name="8" id="8">8</option>
+        </select>
+    </form>
+
+</body>
+</html>
