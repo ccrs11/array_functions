79b388397a90a477fed160b98d2adee1a6de0ce0
ccrs11 <ccrs11@hotmail.com>
2023-06-06 16:45:09 -0500

    feat: :sparkles: exercise 12 finished

--- index.php
+++ index.php
@@ -44,35 +44,39 @@ else if (isset($_POST["ej3"])) {
     $array_tipos = ["sobrevuelo", "orbiter", "atmosférica", "lander", "rover", "penetradora", "observatorio", "comunicaciones"];
     $consulta = in_array($ej7, $array_tipos);
     $res7 = $consulta ? "El tipo de nave $ej7 SI existe" : "El tipo de nave $ej7 NO existe";
-}
-else if (isset($_POST["ej8"])) {
+} else if (isset($_POST["ej8"])) {
     $ej8 = $_POST["ej8"];
     $array_planetas = [1 => "mercurio", 2 => "venus", 3 => "tierra", 4 => "marte", 5 => "júpiter", 6 => "saturno", 7 => "urano", 8 => "neptuno"];
     $al = array_rand($array_planetas);
     $planeta = $array_planetas[$al];
     $res8 = "El indice es: " . $al . " y corresponde al planeta $planeta";
-}
-else if (isset($_POST["ej9"])) {
+} else if (isset($_POST["ej9"])) {
     $ej9 = $_POST["ej9"];
-    $especies = ["klingons", "wookiees","vulcans", "wookiees","daleks","ewoks","xenomorphs","predators", "klingons" ,"daleks"];
-    $unique=array_unique($especies);
-    $encoded=json_encode($unique);
-    $res9 = "Las especies únicas son:  $encoded" ;
+    $especies = ["klingons", "wookiees", "vulcans", "wookiees", "daleks", "ewoks", "xenomorphs", "predators", "klingons", "daleks"];
+    $unique = array_unique($especies);
+    $encoded = json_encode($unique);
+    $res9 = "Las especies únicas son:  $encoded";
+} else if (isset($_POST["ej10"])) {
+    $sistema1 = ["mercurio", "venus", "tierra", "marte", "jupiter", "saturno", "urano", "neptuno"];
+    $sistema2 = ["andromeda", "triangulum", "sombrero", "Whirlpool", "Centaurus", "venus", "tierra", "saturno", "urano"];
+    $inter = array_intersect($sistema1, $sistema2);
+    $encoded = json_encode($inter);
+    $res10 = "Los planetas comunes son: $encoded";
+} else if (isset($_POST["ej11"])) {
+    $sistema1 = ["mercurio", "venus", "tierra", "marte", "jupiter", "saturno", "urano", "neptuno"];
+    $sistema2 = ["andromeda", "triangulum", "sombrero", "Whirlpool", "Centaurus", "venus", "tierra", "saturno", "urano"];
+    $differ = array_diff($sistema1, $sistema2);
+    $encoded = json_encode($differ);
+    $res11 = "Los planetas comunes son: $encoded";
+} else if (isset($_POST["ej12"])) {
+    $ej12 = strtolower($_POST["ej12"]);
+    $array_satelites = ["Europa","Ganimedes","Calisto","Amaltea","Himalia","Elara","Leda","Carme"];
+    $push = array_push($array_satelites,$ej12);
+    var_dump($push);
+    $encoded = json_encode($array_satelites);
+    $res12 = "los satelites de saturno ahora son: $encoded";
 }
-else if(isset($_POST["ej10"])){
-    $sistema1=["mercurio", "venus", "tierra", "marte", "jupiter", "saturno", "urano", "neptuno"];
-    $sistema2=["andromeda","triangulum","sombrero","Whirlpool","Centaurus","venus", "tierra", "saturno", "urano"];
-    $inter=array_intersect($sistema1,$sistema2);
-    $encoded=json_encode($inter);
-    $res10 = "Los planetas comunes son: $encoded" ;
-}
-else if(isset($_POST["ej11"])){
-    $sistema1=["mercurio", "venus", "tierra", "marte", "jupiter", "saturno", "urano", "neptuno"];
-    $sistema2=["andromeda","triangulum","sombrero","Whirlpool","Centaurus","venus", "tierra", "saturno", "urano"];
-    $differ=array_diff($sistema1,$sistema2);
-    $encoded=json_encode($differ);
-    $res11 = "Los planetas comunes son: $encoded" ;
-}
+
 
 
 ?>
@@ -189,7 +193,7 @@ else if(isset($_POST["ej11"])){
     <p> Eliminar elementos duplicados de una lista de especies alienígenas </p>
     <form method="POST" action="">
         <label for="ej9">tocar el botón para seleccionar un planeta aleatorio</label>
-        <button name="ej9">Eliminar duplicados</button>
+        <button name="ej9">Eliminar duplicados</button>`
     </form>
     <h4> <?= $res9 ?></h4>
 
@@ -208,6 +212,16 @@ else if(isset($_POST["ej11"])){
         <button name="ej11">Planetas únicos</button>
     </form>
     <h4> <?= $res11 ?> </h4>
+
+    <h2>Ejercicio 12</h2>
+    <p> Crear una lista de satélites para un planeta </p>
+    <form method="POST" action="">
+        <label for="ej12">Insertar Satelite de jupiter</label>
+        <br>
+        <input type="text" name="ej12">Coloca el nombre del satelite de jupiter que deseas añadir</input>
+    </form>
+    <h4> <?= $res12 ?></h4>
+
 
 </body>
 
